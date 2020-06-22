c9c69405b38b851064102158014b9e9f1191745a
loreanvictor <ghanizadeh.eugene@gmail.com>
2020-06-22 18:47:20 +0000

    Deploying to gh-pages from  @ 561ba8062e42bbde1fdfa530ec75b05fec5dec2e 🚀

--- bundle/codedoc-bundle.entry.js
+++ bundle/codedoc-bundle.entry.js
@@ -13,24 +13,24 @@ import { loadDeferredIFrames } from '/home/runner/work/coding-blog-plugin/coding
 import { smoothLoading } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
 import { tocHighlight } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
 import { postNavSearch } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
-import { TabSelector } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
+import { ArticleCard } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/src/components/article-card/index.tsx';
 import { ToCPrevNext } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
 import { Author } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/src/components/author/index.tsx';
 import { GithubSearch } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
 import { ToCToggle } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
 import { ConfigTransport } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { ArticleCard } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/src/components/article-card/index.tsx';
+import { TabSelector } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
 
 const components = {
-  'CYhYs5vMVDdVPeJ0+jPqeQ==': TabSelector,
+  'kirgXyOKpGUcVTtMGNZNPw==': ArticleCard,
   'yGj+BsApJm+7rG/qLRy4qw==': ToCPrevNext,
   'OjkX2E8f1ZDwZq64A9gksw==': Author,
   'SZElPgAGFBlkhr0p0JFwfw==': GithubSearch,
   'R/uKF3kiw5FN/qXQwgZyvQ==': ToCToggle,
   '0Isz95HRV4teCWLkt5BV5Q==': DarkModeSwitch,
   'pb454kaQbNEgaLB6Jxyx9w==': ConfigTransport,
-  'kirgXyOKpGUcVTtMGNZNPw==': ArticleCard
+  'CYhYs5vMVDdVPeJ0+jPqeQ==': TabSelector
 };
 
 const renderer = getRenderer();
