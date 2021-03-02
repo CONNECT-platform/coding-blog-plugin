50640ffd29584e4939a0c9c3862e08b20df44d42
loreanvictor <ghanizadeh.eugene@gmail.com>
2021-03-02 09:30:39 +0000

    Deploying to gh-pages from  @ 8d6c5b83237432a762885b5a10529ee00a8c1981 🚀

--- bundle/codedoc-bundle.entry.js
+++ bundle/codedoc-bundle.entry.js
@@ -14,6 +14,7 @@ import { smoothLoading } from '/home/runner/work/coding-blog-plugin/coding-blog-
 import { tocHighlight } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
 import { postNavSearch } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
 import { copyLineLinks } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
 import { GithubSearch } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
 import { ToCToggle } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/runner/work/coding-blog-plugin/coding-blog-plugin/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
